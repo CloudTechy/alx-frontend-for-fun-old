aeea2bc9d77216d3f763a1147abecbba80640b9c
Onyekelu Chukwuebuka <onyekeluchukwuebuka@yahoo.ca>
2024-08-06 11:21:40 +0100

    done

---
+++ markdown2html.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A Markdown script that takes an argument of 2 strings
+and generates HTML files"""
+
+import sys
+import os
+
+if __name__ == "__main__":
+    """Main
+    """
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
+        sys.exit(1)
+
+    if not os.path.exists(sys.argv[1]):
+        print("Missing {}".format(sys.argv[1]), file=sys.stderr)
+        sys.exit(1)
+    sys.exit(0)
