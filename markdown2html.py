a66077ee0a7e2c0579a8a0c56a575f400d2fb722
Onyekelu Chukwuebuka <onyekeluchukwuebuka@yahoo.ca>
2024-08-10 04:19:06 +0100

    fixed

--- markdown2html.py
+++ markdown2html.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html",
               file=sys.stderr)
-        sys.exit(1)
+        exit(1)
 
     if not os.path.exists(sys.argv[1]):
         print("Missing {}".format(sys.argv[1]), file=sys.stderr)
-        sys.exit(1)
-    sys.exit(0)
+        exit(1)
+    exit(0)
